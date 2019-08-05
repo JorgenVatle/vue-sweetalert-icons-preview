38088c2432ce24cba8ecb42420a170aff9190feb
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 02:23:00 +0300

    Stylize Nuxt index page, adding descriptions to icons.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="container py-8">
-        <h1 class="text-3xl">Icons</h1>
-        <div v-for="(icon, index) in $options.icons">
-            <sweetalert-icon :icon="icon.name" :key="icon + index" />
+        <h1 class="text-3xl">Vue-Sweetalert-Icons</h1>
+        <div class="grid grid-columns-3 py-8 grid-gap-4">
+            <div v-for="(icon, index) in $options.icons">
+                <sweetalert-icon :icon="icon.name" :key="icon + index" class="mb-2" />
+                <div class="text-center">
+                    <div>
+                        <label class="text-xl capitalize" v-text="icon.name"></label>
+                    </div>
+                    <code class="text-gray-600">&lt;sweetalert-icon icon="{{icon.name}}" /&gt;</code>
+                </div>
+            </div>
         </div>
     </div>
 </template>
