2f8bd684facdd88454eb166977620ff05a5b2161
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 02:38:24 +0300

    Add default link style to example app.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,13 @@
     <nuxt />
   </div>
 </template>
+
+<style lang="scss">
+    a {
+        @apply font-medium text-gray-500;
+
+        &:hover {
+            @apply text-gray-600;
+        }
+    }
+</style>
