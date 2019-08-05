f11f64a6ace7ce62ab43ec7fbbe7a984d871b703
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 02:12:49 +0300

    Add tailwindcss-grid to Tailwind plugins.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,21 @@ module.exports = {
         }
     },
     variants: {},
-    plugins: [],
+    plugins: [require('tailwindcss-grid')({
+        grids: [1, 2, 3, 5, 6, 8, 10, 12],
+        gaps: {
+            0: '0',
+            4: '1rem',
+            8: '2rem',
+            10: '2.5rem',
+            '4-x': '1rem',
+            '4-y': '1rem',
+        },
+        autoMinWidths: {
+            '16': '4rem',
+            '24': '6rem',
+            '300px': '300px',
+        },
+        variants: ['responsive'],
+    })],
 };
