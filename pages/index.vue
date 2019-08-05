11a8fef044d7cf5bf9d2273fba43afa25cc3cc1c
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 00:52:59 +0300

    Render available SweetAlert icons on Nuxt index page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,30 @@
 <template>
-    <div class="container">
-        <h1 class="text-lg">Icons</h1>
+    <div class="container py-8">
+        <h1 class="text-3xl">Icons</h1>
+        <div v-for="(icon, index) in $options.icons">
+            <sweetalert-icon :icon="icon.name" :key="icon + index" />
+        </div>
     </div>
 </template>
+
+<script>
+    export default {
+        icons: [
+            {
+                name: 'success',
+            },
+            {
+                name: 'warning',
+            },
+            {
+                name: 'error',
+            },
+            {
+                name: 'info',
+            },
+            {
+                name: 'loading',
+            }
+        ]
+    }
+</script>
