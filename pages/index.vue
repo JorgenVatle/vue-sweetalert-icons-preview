4d83031a26c044a673f8e4149706c486f989eda9
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 02:25:46 +0300

    Improve responsiveness of Nuxt index page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container py-8">
         <h1 class="text-3xl">Vue-Sweetalert-Icons</h1>
-        <div class="grid grid-columns-3 py-8 grid-gap-4">
-            <div v-for="(icon, index) in $options.icons">
+        <div class="grid md:grid-columns-3 py-4 md:py-8 md:grid-gap-4">
+            <div v-for="(icon, index) in $options.icons" class="py-4 md:py-0">
                 <sweetalert-icon :icon="icon.name" :key="icon + index" class="mb-2" />
                 <div class="text-center">
                     <div>
