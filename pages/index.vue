17440151ec2d4930042089bb3dc8372ecb9dbdd4
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 02:44:55 +0300

    Add author and GitHub link to example index page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
 <template>
     <div class="container py-8">
-        <h1 class="text-3xl">Vue-Sweetalert-Icons</h1>
+
+        <!-- Heading -->
+        <header class="flex justify-between">
+            <!-- Title -->
+            <h1 class="text-3xl">Vue-Sweetalert-Icons</h1>
+
+            <!-- GitHub & Author -->
+            <nav class="text-gray-500 text-right">
+                <a class="font-medium text-gray-500 leading-tight"
+                   href="https://github.com/JorgenVatle/vue-sweetalert-icons">
+                    View on GitHub 🐱‍💻
+                </a>
+                <p class="leading-tight">
+                    <small>
+                        <label>Author</label>
+                        <a href="https://github.com/JorgenVatle" target="_blank" class="font-medium">Jørgen Vatle</a>
+                    </small>
+                </p>
+            </nav>
+        </header>
+
+        <!-- Icon Grid -->
         <div class="grid md:grid-columns-3 py-4 md:py-8 md:grid-gap-4">
             <div v-for="(icon, index) in $options.icons" class="py-4 md:py-0">
-                <sweetalert-icon :icon="icon.name" :key="icon + index" class="mb-2" />
+                <sweetalert-icon :icon="icon.name" :key="icon + index" class="mb-2"/>
                 <div class="text-center">
                     <div>
                         <label class="text-xl capitalize" v-text="icon.name"></label>
@@ -32,7 +53,7 @@
             },
             {
                 name: 'loading',
-            }
-        ]
-    }
+            },
+        ],
+    };
 </script>
