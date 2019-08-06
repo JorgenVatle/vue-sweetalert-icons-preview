75eaa4e7be7b41102062d1e04e0d5ee3a6dd15a9
Jørgen Vatle <Jorgen@WcAServices.net>
2019-08-06 04:33:44 +0300

    Update default meta title and description.

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,11 @@ export default {
     ** Headers of the page
     */
     head: {
-        title: process.env.npm_package_name || '',
+        title: 'Vue SweetAlert Icons',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+            { hid: 'description', name: 'description', content: 'Clean animated status icons for your Vue project. Based on SweetAlert\'s status icons' },
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
